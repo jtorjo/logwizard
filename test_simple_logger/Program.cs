a7d65e0536a5ee27a0ee2d725ee508a80182b7b1
jtorjo <john.groups@torjo.com>
2015-11-07 01:11:43 +0200

    1.4.8g
    - synchronize colors - sync just with the current view by default
    - created simple project for simulating xml logger

---
+++ test_simple_logger/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using log4net;
+
+namespace test_simple_logger {
+    class Program {
+        static void Main(string[] args) {
+            const int sleep_ms = 1000;
+
+            log4net.Config.XmlConfigurator.Configure( new FileInfo("test_simple_logger.exe.config"));
+            var logger = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+            int idx = 0;
+            while (true) {
+                logger.Info("another sample msg " + idx);
+                ++idx;
+                Thread.Sleep(sleep_ms);
+            }
+        }
+    }
+}
